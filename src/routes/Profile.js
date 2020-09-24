02ec372aa27de2420bd2f482ccda81c4cf093306
kylekoh <ykoh728@gmail.com>
2020-09-24 10:29:38 +0900

    make query for firestore

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { authService } from "fbase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "fbase";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+export default ({ userObj }) => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
 
-export default () => {
-  const onLogOutClick = () => authService.signOut();
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, [getMyNweets]);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log out</button>
